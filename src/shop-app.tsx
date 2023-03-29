31dcdb6e2fc5ac0f54db60a4ce8eb6a93931cc16
Sufyan <sufyanaslam2728@gmail.com>
2023-03-30 01:02:08 +0500

    Api calling and refactor

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -9,6 +9,7 @@ import logo from "./images/droppe-logo.png";
 import img1 from "./images/img1.png";
 import img2 from "./images/img2.png";
 import styles from "./shopApp.module.css";
+import { fetchProducts, updateProduct } from "./services/api";
 
 export class ShopApp extends React.Component<
   {},
@@ -20,93 +21,76 @@ export class ShopApp extends React.Component<
     this.favClick = this.favClick.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
+    //removed api call from constructor as its a bad practice
     this.state = { products: [], isOpen: false, isShowingMessage: false, message: '', numFavorites: 0, prodCount: 0 };
-
-    fetch('https://fakestoreapi.com/products').then((response) => {
-      let jsonResponse = response.json();
-
-      jsonResponse.then((rawData) => {
-        let data = [];
-
-        for (let i = 0; i < rawData.length; i++) {
-          let updatedProd = rawData[i];
-          data.push(updatedProd);
-        }
-        this.setState({
-          products: data,
-        });
-        this.setState({
-          prodCount: data.length
-        })
-      });
-    });
   }
-
-   componentDidMount(){
-      document.title = "Droppe refactor app"
-   }
+  async componentDidMount(){
+    document.title = "Droppe refactor app"
+    
+    /*
+      moved api call here and wrapped in try catch in to handle crash in case of api call failure
+      api calls moved to separate network layer to increase the reusability, modularity and easy implementation of unit-tests.
+    */
+    
+    try {
+      let productList = await fetchProducts()
+      this.setState({
+        products: productList
+      })
+      this.setState({
+        prodCount: productList.length
+      })
+    } catch (error) {}
+  }
 
   favClick(title: string) {
     const prods = this.state.products;
-    const idx = lodash.findIndex(prods, {title: title})
+    const idx = lodash.findIndex(prods, {title: title});
     let currentFavs = this.state.numFavorites
     let totalFavs: any;
 
     if (prods[idx].isFavorite) {
       prods[idx].isFavorite = false;
-      totalFavs = --currentFavs
+      totalFavs = --currentFavs;
     } else {
-      totalFavs = ++currentFavs
+      totalFavs = ++currentFavs;
       prods[idx].isFavorite = true;
     }
 
     this.setState(() => ({ products: prods, numFavorites: totalFavs }));
   }
 
-  onSubmit(payload: { title: string; description: string, price: string }) {
-    const updated = lodash.clone(this.state.products);
-    updated.push({
-      title: payload.title,
-      description: payload.description,
-      price: payload.price
-    });
-
+  async onSubmit(payload: {title: string; description: string; price: string}) {
+    //updated the flow for product update, state should only update if the product is actually updated, removed timeout from message
     this.setState({
-      products: updated,
-      prodCount: lodash.size(this.state.products) + 1
-    });
+      isShowingMessage: true,
+      message: "Adding product..."
+    })
+
+    try {
+      let result = await updateProduct(payload)
+      const updated = lodash.clone(this.state.products)
+
+      if (result?.id) {
+        updated.push({
+          id: result.id,
+          title: payload.title,
+          description: payload.description,
+          price: payload.price
+        })
+
+        this.setState({
+          products: updated,
+          prodCount: lodash.size(this.state.products) + 1
+        })
+      }
+    } catch (error) {}
 
     this.setState({
       isOpen: false,
-    });
-
-    this.setState({
-      isShowingMessage: true,
-      message: 'Adding product...'
+      isShowingMessage: false,
+      message: ""
     })
-
-    // **this POST request doesn't actually post anything to any database**
-    fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    title: payload.title,
-                    price: payload.price,
-                    description: payload.description,
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json => {
-               (function (t) {
-                 setTimeout(()=>{
-                    t.setState({
-                       isShowingMessage: false,
-                       message: ''
-                    })
-                 }, 2000)
-              })(this);
-            })
   }
 
   render() {
